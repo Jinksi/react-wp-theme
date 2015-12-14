38f2841e6b8b92600deb11523a17291fa767eb55
Jinksi <ericjinks@gmail.com>
2015-12-14 21:10:03 +1000

    Init

---
+++ app.jsx
@@ -0,0 +1,12 @@
+var App = React.createClass({
+  render: function(){
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
